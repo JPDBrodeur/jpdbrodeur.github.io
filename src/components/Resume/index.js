edb9d5e41e2273758905edd5d99b2870816897e5
JPDBrodeur <jbrodeur001@gmail.com>
2022-01-07 00:05:22 -0500

    build for GitHub Pages

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,6 +1,10 @@
 function Resume() {
   return (
-    <></>
+    <div className="flex justify-center">
+      <a href="https://jpdbrodeur.github.io/assets/docs/resume.pdf">
+        <button data-testid="button" target="_blank" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">Download Resume</button>
+      </a>
+    </div>
   );
 }
 
