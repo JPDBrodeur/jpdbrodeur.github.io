eddf3d997766f0a539c0d274693a9b2456a613ae
JPDBrodeur <jbrodeur001@gmail.com>
2022-06-22 20:40:18 -0400

    homepage link fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,7 +6,7 @@ function Header({ setCurrentSection, currentSection }) {
   return (
     <header className="p-5 bg-gray-100">
       <div className="flex flex-col text-center lg:flex-row justify-between">
-        <a href="/portfolio"><h1 className="text-5xl font-bold">John Brodeur</h1></a>
+        <a href="/"><h1 className="text-5xl font-bold">John Brodeur</h1></a>
         <Nav setCurrentSection={setCurrentSection} currentSection={currentSection} />
       </div>
     </header>
