7159d4bb921a715044370c8def26862213923a8d
JPDBrodeur <jbrodeur001@gmail.com>
2022-06-22 23:01:24 -0400

    adjusted header height

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,7 @@ import Nav from '../../components/Navigation'
 function Header({ setCurrentSection, currentSection }) {
 
   return (
-    <header className="p-5 bg-gray-100 shadow-md">
+    <header className="pt-5 px-5 pb-1 lg:p-5 bg-gray-100 shadow-md">
       <div className="flex flex-col text-center lg:flex-row justify-between">
         <a href="/"><h1 className="text-5xl font-bold">John Brodeur</h1></a>
         <Nav setCurrentSection={setCurrentSection} currentSection={currentSection} />
